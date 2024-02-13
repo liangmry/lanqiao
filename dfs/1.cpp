5e3e034b6508825123f283fc73ad373506a562ae
lbh <3558506522@qq.com>
2024-02-13 13:33:54 +0800

    接龙序列从暴力到记忆化逐步推导

--- dfs/1.cpp
+++ dfs/1.cpp
@@ -1,25 +1,116 @@
-/*
- * 接龙数列
- * 对于一个长度为 K 的整数数列：A1, A2, . . . , AK，我们称之为接龙数列当且仅当 Ai 的首位数字恰好等于 Ai−1 的末位数字 (2 ≤ i ≤ K)。
-例如 12, 23, 35, 56, 61, 11 是接龙数列；12, 23, 34, 56 不是接龙数列，因为 56的首位数字不等于 34 的末位数字。所有长度为 1 的整数数列都是接龙数列。
-现在给定一个长度为 N 的数列 A1, A2, . . . , AN，请你计算最少从中删除多少个数，可以使剩下的序列是接龙序列？
- */
-//动态规划
-#include <bits/stdc++.h>
+// P9242 [蓝桥杯 2023 省 B] 接龙数列
+#include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
-int n, dp[10];
-int main()
-{
-    cin >> n;
-    int maxn=-1;
-    for(int i=0; i<n; i++)
-    {
-        string s;
-        cin >> s;
-        int a=s[0]-'0', b=s.back()-'0';
-        dp[b]=max(dp[b], dp[a]+1);
-        maxn = max(maxn, dp[b]);
+
+const int N = 1e5+5;
+int n; // 数列的长度
+string s[N];
+int cnt = -1;
+
+int get_end(string s) {
+    return *(s.end()-1) - '0';
+}
+
+int get_first(string s) {
+    return *(s.begin()) - '0';
+}
+
+/* 1.
+ * char end_ch 我想用char来判断选不选，但是 == '' 不能用，改用int来判断
+void dfs(int x, char end_ch, int plus_cnt) { // 对第x个字符串做选择  当前数列的最后一个字符
+    if(x > n) {
+        cnt = max(cnt, plus_cnt);
+        return ;
     }
-    cout << n-maxn;
+    if(end_ch == *(s[x].begin()) or plus_cnt == 0) { // 第x个字符串可以接龙
+        dfs(x+1, *(s[x].end()-1), plus_cnt+1);
+    }
+    else {
+        dfs(x+1, end_ch, plus_cnt);
+    }
+}
+ */
+
+
+/* 2.
+ * void dfs(int x, int end, int plus_cnt) {
+    if(plus_cnt + (n-x+1) <= cnt) return ; // 剩下的就算全选，也不必cnt大
+    if(x > n) {
+        cnt = max(cnt, plus_cnt);
+        return ;
+    }
+    if(end == -1 or get_first(s[x]) == end) { // 当前没选任何一个数 or 可以接龙
+        dfs(x+1, get_end(s[x]), plus_cnt+1); // 选择s[x]
+    }
+    dfs(x+1, end, plus_cnt); // 不选s[x]
+}
+ */
+
+
+/* 3. 为了做记忆化，对 2 的 void 改成返回值类型为 int
+ * int dfs(int x, int end) {
+    if(x > n) {
+        return 0;
+    }
+    if(end == -1 or get_first(s[x]) == end) {
+        return max(dfs(x+1, get_end(s[x]))+1, dfs(x+1, end));
+    }
+    return dfs(x+1, end);
+}
+ *
+ */
+
+
+/*  4. 记忆化
+ * const int N = 1e5+5;
+int n; // 数列的长度
+string s[N];
+
+int mem[N][12];
+
+int get_end(string s) {
+    return *(s.end()-1) - '0';
+}
+
+int get_first(string s) {
+    return *(s.begin()) - '0';
+}
+
+int dfs(int x, int end) {
+    if(mem[x][end] != -1) return mem[x][end];
+    int num = 0;
+    if(x > n) {
+        num =  0;
+    }
+    else if(end == 10 or get_first(s[x]) == end) {
+        num =  max(dfs(x+1, get_end(s[x]))+1, dfs(x+1, end));
+    }
+    else if(get_first(s[x]) != end)
+        num =  dfs(x+1, end);
+    mem[x][end] = num;
+    return mem[x][end];
+}
+
+
+int main() {
+    memset(mem, -1, sizeof mem);
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        cin >> s[i];
+    int cnt = dfs(1, 10);
+    cout << n - cnt;
+    return 0;
+}
+ */
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        cin >> s[i];
+//    dfs(1, -1, 0);
+    cout << n - cnt;
     return 0;
 }
