7b99069c31d3e29689be015e0485b7fd552c63c9
lbh <3558506522@qq.com>
2024-02-13 22:46:03 +0800

    指定第一组枚举数列的全排列问题

--- dfs/5.cpp
+++ dfs/5.cpp
@@ -1,23 +1,25 @@
-#include "bits/stdc++.h"
+// P1088 [NOIP2004 普及组] 火星人
+#include <iostream>
+
 using namespace std;
-int n; //火星人给的数字总数
-int m; //要加的数
-const int N=10003;
-bool st[N];//枚举数的状态
-int mars[N];//火星人输入的数
-int res = 0, arr[N]; //已经选好的排列数
+// 全排列问题，但是 首次枚举 的一组数字需要 指定
+int n, m; // n 火星人的手指数， m 另外要加上的一个数字
+const int N = 10005;
+int mars[N]; // 火星人给定的第一组数字
+bool st[N];
+int arr[N]; // 保存输出
+int cnt;
 bool ff;
-void dfs(int x)
-{
-    if(ff)
-        return ;
+
+void dfs(int x) {
+    if(ff) return ; // 已经找到了，不要再向后枚举，因此时间复杂度得以降低
 
     if(x > n)
     {
-        res++;
-        if(res == m+1)
+        cnt++;
+        if(cnt == m+1) // m+1 的原因：火星人给定的第一组数列 cnt为 1
         {
-            ff=true;
+            ff=true; // 找到了那组数列
             for(int i=1; i<=n; i++)
             {
                 cout << arr[i] << " ";
@@ -26,30 +28,26 @@ void dfs(int x)
         return ;
     }
 
-    for(int i=1; i<=n; i++)
-    {
-        if(!res)
-        {
-            i = mars[x];
+    for(int i = 1; i <= n; i++) {
+        if(cnt == 0) {
+            i = mars[x]; // 很重要！！好好体会
         }
-        if(!st[i])
-        {
+        if(!st[i]) {
             st[i] = true;
-            arr[x] = i;
+            arr[x] = i; // 很重要！！好好体会
             dfs(x+1);
+
+            // 恢复状态
             st[i] = false;
-            arr[x] = 0;
+            arr[x] = 0; // 可以省略
         }
     }
 }
-int main()
-{
-    cin>>n>>m;
-    for(int i=1; i<=n; i++)
-    {
-        cin>>mars[i];
-    }
-    dfs(1);
 
+int main() {
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++)
+        cin >> mars[i];
+    dfs(1);
     return 0;
 }
