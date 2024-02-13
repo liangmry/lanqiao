1dbf842e3011dda36feb6a674a2a0507eb3f0010
lbh <3558506522@qq.com>
2024-02-13 19:51:00 +0800

    指数型枚举

--- dfs/9.cpp
+++ dfs/9.cpp
@@ -1,41 +1,33 @@
-#include<iostream>
+// 递归实现指数型枚举 https://www.acwing.com/problem/content/94/
+#include <iostream>
+
 using namespace std;
 
-// 指数级枚举
-
 int n;
-const int N = 20;
-int st[N];//状态 0未定  1不选  2选
+const int N = 17;
+bool st[N];
 
-void dfs(int x)
-{
-    if(x > n)
-    {
-        for(int i = 1; i <= n; i++)
-        {
-            if(st[i] == 2)
-            {
-                cout << i << " ";
-            }
+void dfs(int x) { // 第x个数要不要选
+    if(x > n) { // 选择完毕
+        for(int i = 1; i <= n; i++) {
+            if(st[i]) cout << i << " ";
         }
         cout << endl;
         return ;
     }
-
-    //不选
-    st[x] = 1;
+    // 不选, 有些步骤其实不用写
+    st[x] = false;
     dfs(x+1);
-    st[x]=0; //恢复状态
+    st[x] = false; // 恢复状态
 
-    //选
-    st[x] = 2;
+    // 选
+    st[x] = true;
     dfs(x+1);
-    st[x] = 0;
+    st[x] = false;
 }
 
-int main()
-{
+int main() {
     cin >> n;
-    dfs(1); //第一个位置
+    dfs(1);
     return 0;
 }
